dd5368ff0d53d55abe398616359b6d94c9fef733
priaaji-seyanegoro <priaaji.setyanegoro@gmail.com>
2024-03-30 04:17:55 +0700

    changes typo pricing list text

--- src/constants/prices.tsx
+++ src/constants/prices.tsx
@@ -1,12 +1,12 @@
 export const pricesMonthly = [
     {
         name: "start",
-        features: ["10x Prompt Code Generation", "10x Prompt Code Assitant "]
+        features: ["10x Prompt Image to Code", "10x Prompt Code Assistance "]
     },
     {
         name: "premium",
         price: "0.1 ETH",
         popular: true,
-        features: ["Unlimited Prompt Code Generation", "Unlimited Prompt Code Assitant", 'Automate Web Deployer', "24/7 developer services", 'Fastest Response Help Desk']
+        features: ["Unlimited Prompt Image to Code", "Unlimited Prompt Code Assistance", 'Automate Web Deployer', "24/7 developer services", 'Fastest Response Help Desk']
     }
 ];
