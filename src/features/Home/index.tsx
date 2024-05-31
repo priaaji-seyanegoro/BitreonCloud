1e5f60d7334b136af89007e4e990637b7ca3edaf
priaaji-seyanegoro <priaaji.setyanegoro@gmail.com>
2024-06-01 04:35:42 +0700

    add btn and second teks welcome

--- src/features/Home/index.tsx
+++ src/features/Home/index.tsx
@@ -51,37 +51,62 @@ const Home: React.FC<Props> = () => {
       <div className={` homepage-container ${kanit.className}`}>
         <div
           id="welcome"
-          className="h-[6em] lg:h-[6em] xl:h-[15em] relative z-30"
+          className="h-[7em] lg:h-[6em] xl:h-[15em] relative z-30"
         />
 
         <Image
           src={welcomeBackground}
           alt="roadmap"
           priority
-          className="w-[100%] h-[300px] lg:h-[auto] absolute top-0 left-0 bg-cover bg-center bg-no-repeat"
+          className="w-[100%] h-[350px] lg:h-[auto] absolute top-0 left-0 bg-cover bg-center bg-no-repeat"
         />
 
         <div className="welcome-container lg:mt-15 max-w-screen-xl mx-auto">
           <div className="welcome-wrapper">
-            {/* <div
-              className={`w-full h-full ${kanit.className}`}
-            >
-              <Text
-                fontSize="108px"
-                fontWeight="700px"
-                textTransform="uppercase">
-                secure your digital surfing with confidence
-              </Text>
-  
-            </div> */}
 
-            <div
-              className={clsx(
-                ' text-center mx-auto text-sm w-[200px] lg:w-[850px] lg:text-6xl 2xl:text-8xl font-bold text-white py-2 uppercase',
-                kanitBold.className
-              )}
-            >
-              secure <span className="bg-gradient-to-r from-[#fff0] to-[#ffff] bg-clip-text text-transparent">your</span> digital surfing with <span className="bg-gradient-to-r from-[#fff0] to-[#ffff] bg-clip-text text-transparent">confidence</span>
+            <div className={`w-full sm:px-8 lg:px-14`}>
+              <div
+                className={clsx(
+                  'text-center mx-auto  w-[300px] lg:w-[850px] text-3xl lg:text-8xl font-bold text-white uppercase m-0 p-0 mb-2 leading-tight',
+                  kanitBold.className
+                )}
+              >
+                Reinventing Data Security and Privacy
+              </div>
+              {/* <div
+                className={`w-full lg:w-4/5 text-center mx-auto text-4xl lg:text-6xl 2xl:text-6xl font-bold text-white py-2 leading-10 uppercase ${kanitBold.className
+                  }`}
+              >
+                Reinventing Data Security and Privacy
+              </div> */}
+
+              <div
+                className={clsx(
+                  'text-center mx-auto w-[180px] lg:w-[650px] text-[5px] lg:text-[15px] text-[#CBCBCB] uppercase leading-tight m-0',
+                  kanit.className
+                )}
+              >
+                Experience secure and scalable privacy protection with Power Link's advanced VPN, ensuring your data remains confidential and secure.
+              </div>
+
+              <div className="text-center mx-auto mt-2 lg:mt-6 ">
+                <Link
+                  href={socialsLink.telegram}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Button
+                    className={clsx(
+                      kanitBold.className,
+                      'w-[70px] lg:w-[174px] text-[8px] lg:text-lg bg-gradient-to-r from-[#4260FF] to-[#0024E1] rounded-full'
+                    )}
+                    variant="outline"
+                    size={{ base: "xs", lg: 'lg' }}
+                  >
+                    Choose Plan
+                  </Button>
+                </Link>
+              </div>
             </div>
 
           </div>
